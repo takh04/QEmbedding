4c36d9478707c77dc0ec8f6f3ed8b42cd5566315
takh04 <takh0404@gmail.com>
2022-11-28 13:47:17 +0900

    Quantum Embedding

---
+++ embedding.py
@@ -0,0 +1,83 @@
+import pennylane as qml
+N_layers = 3
+
+def exp_Z(x, wires, inverse=False):
+  if inverse == False:
+    qml.RZ(-2 * x, wires=wires)
+  elif inverse == True:
+    qml.RZ(2 * x, wires=wires)
+
+def exp_ZZ1(x, wires, inverse=False):
+  if inverse == False:
+    qml.CNOT(wires=wires)
+    qml.RZ(-2 * x, wires=wires[1])
+    qml.CNOT(wires=wires)
+  elif inverse == True:
+    qml.CNOT(wires=wires)
+    qml.RZ(2 * x, wires=wires[1])
+    qml.CNOT(wires=wires)
+
+def exp_ZZ2(x1, x2, wires, inverse=False):
+  if inverse == False:
+    qml.CNOT(wires=wires)
+    qml.RZ(-2 * (np.pi - x1) * (np.pi - x2), wires=wires[1])
+    qml.CNOT(wires=wires)
+  elif inverse == True:
+    qml.CNOT(wires=wires)
+    qml.RZ(2 * (np.pi - x1) * (np.pi - x2), wires=wires[1])
+    qml.CNOT(wires=wires)
+
+
+
+# Quantum Embedding 1 for model 1
+def QuantumEmbedding1(input):
+  for i in range(N_layers):
+    for j in range(8):
+      qml.Hadamard(wires=j)
+      exp_Z(input[j], wires=j)
+    for k in range(7):
+      exp_ZZ2(input[k], input[k+1], wires=[k,k+1])
+    exp_ZZ2(input[7], input[0], wires=[7, 0])
+
+def QuantumEmbedding1_inverse(input):
+  for i in range(N_layers):
+    exp_ZZ2(input[7], input[0], wires=[7, 0], inverse=True)
+    for k in reversed(range(7)):
+      exp_ZZ2(input[k], input[k+1], wires=[k,k+1], inverse=True)
+    qml.Barrier()
+    for j in range(8):
+      exp_Z(input[j], wires=j, inverse=True)
+      qml.Hadamard(wires=j)
+
+# Quantum Embedding 2 for model 2
+def QuantumEmbedding2(input):
+  for i in range(N_layers):
+    for j in range(8):
+      qml.Hadamard(wires=j)
+      exp_Z(input[j], wires=j)
+    for k in range(7):
+      exp_ZZ1(input[8+k], wires=[k, k+1])
+    exp_ZZ1(input[15], wires=[7,0])
+
+def QuantumEmbedding2_inverse(input):
+  for i in range(N_layers):
+    exp_ZZ1(input[15], wires=[7,0], inverse=True)
+    for k in reversed(range(7)):
+      exp_ZZ1(input[k+8], wires=[k,k+1], inverse=True)
+    qml.Barrier()
+    for j in range(8):
+      exp_Z(input[j], wires=j, inverse=True)
+      qml.Hadamard(wires=j)
+
+dev = qml.device('default.qubit', wires=8)
+@qml.qnode(dev, interface="torch")
+def circuit1(inputs): # circuit1 for model 1
+  QuantumEmbedding1(inputs[0:8])
+  QuantumEmbedding1_inverse(inputs[8:16])
+  return qml.probs(wires=range(8))
+
+@qml.qnode(dev, interface="torch")
+def circuit2(inputs): # circuit2 for model 2
+  QuantumEmbedding2(inputs[0:16])
+  QuantumEmbedding2_inverse(inputs[16:32])
+  return qml.probs(wires=range(8))
