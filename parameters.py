72b661846e284e8b1e5291fb5e7a3c7cc706b670
takh04 <takh0404@gmail.com>
2022-11-29 15:54:35 +0900

    changed to globally defined parameters.py

--- parameters.py
+++ parameters.py
@@ -4,6 +4,7 @@ model = 'model1'
 measure = 'Fidelity'
 distance_measure = 'Trace'
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+N_layers = 3
 
-def parameters():
-    return model, measure, distance_measure, device
+def get_parameters():
+    return model, measure, distance_measure, device, N_layers
