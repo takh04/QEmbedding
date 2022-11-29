ea1a0ee39dcd23eaea7021f893272defa67e8173
takh04 <takh0404@gmail.com>
2022-11-30 02:36:22 +0900

    Parameters chagned

--- parameters.py
+++ parameters.py
@@ -1,7 +1,7 @@
 import torch
 
 model = 'model1'
-measure = 'HS-inner'
+measure = 'Fidelity'
 distance_measure = 'Trace'
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 N_layers = 3
