8567f5bc35fcd29f7a31cd31c023426b1b51ebbb
takh04 <takh0404@gmail.com>
2022-12-01 03:45:24 +0900

    changes

--- Hybrid_nn.py
+++ Hybrid_nn.py
@@ -4,134 +4,163 @@ from torch import nn
 import embedding
 import parameters
 
-model, measure, distance_measure, device = parameters.model, parameters.measure, parameters.distance_measure, parameters.device
-
+model, measure, device = parameters.model, parameters.measure, parameters.device
 dev = qml.device('default.qubit', wires=8)
+
+
+# Hybrid Model 1
 @qml.qnode(dev, interface="torch")
-def circuit1(inputs): # circuit1 for model 1
+def circuit1(inputs): 
     embedding.QuantumEmbedding1(inputs[0:8])
     embedding.QuantumEmbedding1_inverse(inputs[8:16])
     return qml.probs(wires=range(8))
 
-@qml.qnode(dev, interface="torch")
-def circuit2(inputs): # circuit2 for model 2
-    embedding.QuantumEmbedding2(inputs[0:16])
-    embedding.QuantumEmbedding2_inverse(inputs[16:32])
-    return qml.probs(wires=range(8))
-
-class HybridModel(torch.nn.Module):
+class HybridModel1(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.qlayer1 = qml.qnn.TorchLayer(circuit1, weight_shapes={})
-        self.qlayer2 = qml.qnn.TorchLayer(circuit2, weight_shapes={})
         self.matrix_fn1 = qml.matrix(circuit1)
-        self.matrix_fn2 = qml.matrix(circuit2)
         self.linear_relu_stack1 = nn.Sequential(
             nn.Linear(8, 10),
             nn.ReLU(),
             nn.Linear(10,10),
             nn.ReLU(),
-            nn.Linear(10,8),
-            nn.Softmax()
+            nn.Linear(10,8)
         )
+
+    if measure == 'Fidelity':
+        def forward(self, x1, x2):
+            x1 = self.linear_relu_stack1(x1)
+            x2 = self.linear_relu_stack1(x2)
+            x = torch.concat([x1, x2], 1)
+            x = self.qlayer1(x)
+            return x[:,0]
+    elif measure == 'HS-inner':
+        def forward(self, x1, x2):
+            x1 = self.linear_relu_stack1(x1)
+            x2 = self.linear_relu_stack1(x2)
+            x = torch.concat([x1, x2], 1).to("cpu")
+            print(x.requires_grad)
+            x = [torch.real(torch.trace(self.matrix_fn1(a))) for a in x]
+            x = torch.tensor(x, requires_grad=True).to(device)
+            print(x.is_leaf)
+            return x / 2**8
+
+
+
+# Hybrid Model 2
+@qml.qnode(dev, interface="torch")
+def circuit2(inputs): 
+    embedding.QuantumEmbedding2(inputs[0:16])
+    embedding.QuantumEmbedding2_inverse(inputs[16:32])
+    return qml.probs(wires=range(8))
+
+class HybridModel2(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.qlayer2 = qml.qnn.TorchLayer(circuit2, weight_shapes={})
+        self.matrix_fn2 = qml.matrix(circuit2)
         self.linear_relu_stack2 = nn.Sequential(
             nn.Linear(8, 20),
             nn.ReLU(),
             nn.Linear(20,20),
             nn.ReLU(),
-            nn.Linear(20,16),
-            nn.Softmax()
+            nn.Linear(20,16)
         )
-
-    if model == "model1":
-        if measure == 'Fidelity':
-            def forward(self, x1, x2):
-                x1 = self.linear_relu_stack1(x1)
-                x2 = self.linear_relu_stack1(x2)
-                x = torch.concat([x1, x2], 1)
-                x = self.qlayer1(x)
-                return x[:,0]
-        elif measure == 'HS-inner':
-            def forward(self, x1, x2):
-                x1 = self.linear_relu_stack1(x1)
-                x2 = self.linear_relu_stack1(x2)
-                x = torch.concat([x1, x2], 1).to("cpu")
-                x = [torch.real(torch.trace(self.matrix_fn1(a))) for a in x]
-                x = torch.tensor(x).to(device)
-                return x / 2**8
     
-    elif model == 'model2':
-        if measure == 'Fidelity':
-            def forward(self, x1, x2):
-                x1 = self.linear_relu_stack2(x1)
-                x2 = self.linear_relu_stack2(x2)
-                x = torch.concat([x1, x2], 1)
-                x = self.qlayer2(x)
-                return x[:,0]
-        elif measure == 'HS-inner':
-            def forward(self, x1, x2):
-                x1 = self.linear_relu_stack2(x1)
-                x2 = self.linear_relu_stack2(x2)
-                x = torch.concat([x1, x2], 1).to("cpu")
-                x = [torch.real(torch.trace(self.matrix_fn2(a))) for a in x]
-                x = torch.tensor(x).to(device)
-                return x / 2**8
+    if measure == 'Fidelity':
+        def forward(self, x1, x2):
+            x1 = self.linear_relu_stack2(x1)
+            x2 = self.linear_relu_stack2(x2)
+            x = torch.concat([x1, x2], 1)
+            x = self.qlayer2(x)
+            return x[:,0]
+    elif measure == 'HS-inner':
+        def forward(self, x1, x2):
+            x1 = self.linear_relu_stack2(x1)
+            x2 = self.linear_relu_stack2(x2)
+            x = torch.concat([x1, x2], 1).to("cpu")
+            x = [torch.real(torch.trace(self.matrix_fn2(a))) for a in x]
+            x = torch.tensor(x).to(device)
+            return x / 2**8
 
 
-# Hybrid Model for distance measure
+
+# Hybrid Distance Model1
 @qml.qnode(dev, interface="torch")
 def distance_circuit1(inputs): 
     embedding.QuantumEmbedding1(inputs[0:8])
     return qml.density_matrix(wires=range(8))
 
-@qml.qnode(dev, interface="torch")
-def distance_circuit2(inputs):
-    embedding.QuantumEmbedding2(inputs[0:16])
-    return qml.density_matrix(wires=range(8))
-
-class HybridModel_distance(torch.nn.Module):
+class HybridModel1_distance(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.qlayer1_distance = qml.qnn.TorchLayer(distance_circuit1, weight_shapes={})
-        self.qlayer2_distance = qml.qnn.TorchLayer(distance_circuit2, weight_shapes={})
         self.linear_relu_stack1 = nn.Sequential(
             nn.Linear(8, 10),
             nn.ReLU(),
             nn.Linear(10,10),
             nn.ReLU(),
-            nn.Linear(10,8),
-            nn.Softmax()
+            nn.Linear(10,8)
         )
+
+    def forward(self, x1, x0):
+        x1 = self.linear_relu_stack1(x1)
+        x0 = self.linear_relu_stack1(x0)
+        rhos1 = self.qlayer1_distance(x1)
+        rhos0 = self.qlayer1_distance(x0)
+        rho1 = torch.sum(rhos1, dim=0) / len(x1)
+        rho0 = torch.sum(rhos0, dim=0) / len(x0)
+        rho_diff = rho1 - rho0
+        
+        if measure == 'Trace':
+            eigvals = torch.linalg.eigvals(rho_diff)
+            return -0.5 * torch.real(torch.sum(torch.abs(eigvals)))
+        elif measure == 'Hilbert-Schmidt':
+            return -torch.trace(rho_diff @ rho_diff)
+
+
+
+
+# Hybrid Distance Model 2
+@qml.qnode(dev, interface="torch")
+def distance_circuit2(inputs):
+    embedding.QuantumEmbedding2(inputs[0:16])
+    return qml.density_matrix(wires=range(8))
+
+class HybridModel2_distance(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.qlayer2_distance = qml.qnn.TorchLayer(distance_circuit2, weight_shapes={})
         self.linear_relu_stack2 = nn.Sequential(
             nn.Linear(8, 20),
             nn.ReLU(),
             nn.Linear(20,20),
             nn.ReLU(),
-            nn.Linear(20,16),
-            nn.Softmax()
+            nn.Linear(20,16)
         )
 
     def forward(self, x1, x0):
-        if model == "model1":
-            x1 = self.linear_relu_stack1(x1)
-            x0 = self.linear_relu_stack1(x0)
-            rhos1 = self.qlayer1_distance(x1)
-            rhos0 = self.qlayer1_distance(x0)
-            rho1 = torch.sum(rhos1, dim=0) / len(x1)
-            rho0 = torch.sum(rhos0, dim=0) / len(x0)
-            rho_diff = rho1 - rho0
-        
-        elif model == "model2":
-            x1 = self.linear_relu_stack2(x1)
-            x0 = self.linear_relu_stack2(x0)
-            rhos1 = self.qlayer2_distance(x1)
-            rhos0 = self.qlayer2_distance(x0)
-            rho1 = torch.sum(rhos1, dim=0) / len(x1)
-            rho0 = torch.sum(rhos0, dim=0) / len(x0)
-            rho_diff = rho1 - rho0
+        x1 = self.linear_relu_stack2(x1)
+        x0 = self.linear_relu_stack2(x0)
+        rhos1 = self.qlayer2_distance(x1)
+        rhos0 = self.qlayer2_distance(x0)
+        rho1 = torch.sum(rhos1, dim=0) / len(x1)
+        rho0 = torch.sum(rhos0, dim=0) / len(x0)
+        rho_diff = rho1 - rho0
 
-        if distance_measure == 'Trace':
+        if measure == 'Trace':
             eigvals = torch.linalg.eigvals(rho_diff)
             return -0.5 * torch.real(torch.sum(torch.abs(eigvals)))
-        elif distance_measure == 'Hilbert-Schmidt':
+        elif measure == 'Hilbert-Schmidt':
             return -torch.trace(rho_diff @ rho_diff)
+
+def get_model():
+    if model == 'model1':
+        return HybridModel1()
+    elif model == 'model2':
+        return HybridModel2()
+    elif model == 'model1_distance':
+        return HybridModel1_distance()
+    elif model == 'model2_distance':
+        return HybridModel2_distance()
--- training.py
+++ training.py
@@ -8,7 +8,7 @@ device = parameters.device
 print(f"Uisng Device: {device}\n")
 
 batch_size = 25
-iterations = 5000
+iterations = 1000
 
 #load data
 feature_reduction = 'PCA8'
@@ -27,9 +27,12 @@ for i in range(len(X_test)):
     else:
         X0_test.append(X_test[i])
 
-X1_train, X0_train, X1_test, X0_test = torch.tensor(X1_train).to(device), torch.tensor(X0_train).to(device), torch.tensor(X1_test).to(device), torch.tensor(X0_test).to(device)
+# train, validation, test dataset for hybrid distance model
+X1_train_distance, X0_train_distance = torch.tensor(X1_train).to(device), torch.tensor(X0_train).to(device)
+X1_valid_distance, X0_valid_distance = torch.tensor(X1_test[:300]).to(device), torch.tensor(X0_test[:300]).to(device)
+X1_test_distance, X0_test_distance = torch.tensor(X1_test[300:]).to(device), torch.tensor(X0_test[300:]).to(device)
 
-#make new data
+#make new data for hybrid model
 def new_data(batch_size):
     X1_new, X2_new, Y_new = [], [], []
     for i in range(batch_size):
@@ -42,11 +45,43 @@ def new_data(batch_size):
             Y_new.append(0)
     return torch.tensor(X1_new), torch.tensor(X2_new), torch.tensor(Y_new)
 
-# Train the model
-def train():
-    current_loss, count = 1, 0
-    loss_history = []
-    model = Hybrid_nn.HybridModel().to(device)
+N_valid, N_test = 300, 1000
+X1_new_valid, X2_new_valid, Y_new_valid = new_data(N_valid)
+X1_new_test, X2_new_test, Y_new_test = new_data(N_test)
+
+
+# Early Stopping and Accuracy
+class EarlyStoppter:
+    def __init__(self, patience=1, min_delta=0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.min_validation_loss = np.inf
+
+    def early_stop(self, validation_loss):
+        if validation_loss < self.min_validation_loss:
+            self.min_validation_loss = validation_loss
+            self.counter = 0
+        elif validation_loss > (self.min_validation_loss + self.min_delta):
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+        return False
+
+early_stopper = EarlyStoppter(patience=3, min_delta=0)
+
+def accuracy(predictions, labels):
+    correct = 0
+    for p,l in zip(predictions, labels):
+        if np.abs(p - 1) < 0.1:
+            correct += 1
+    return correct / len(predictions) * 100
+
+
+# Train Hybrid models
+def train_hybrid():
+    train_loss, valid_loss, valid_acc = [], [], []
+    model = Hybrid_nn.get_model().to(device)
     model.train()
 
     loss_fn = torch.nn.MSELoss()
@@ -58,54 +93,96 @@ def train():
         pred = model(X1_batch, X2_batch)
         pred, Y_batch = pred.to(torch.float32), Y_batch.to(torch.float32)
         loss = loss_fn(pred, Y_batch)
-        loss_history.append(loss.item())
-        if parameters.measure == "HS-inner":
-            loss.requires_grad = True
+        train_loss.append(loss.item())
 
         opt.zero_grad()
         loss.backward()
         opt.step()
 
-        if it % 10 == 0:
+        if it % 20 == 0:
             print(f"Iterations: {it} Loss: {loss.item()}")
-        
-        if (current_loss - loss.item()) / current_loss < 0.01:
-            count = count + 1
-            if count == 5:
-                print('Loss convergered!')
-                break
-        else:
-            count = 0
-        
-    torch.save(model.state_dict(), './model_state_dict.pt')
-    f = open("Loss History.txt", 'w')
-    f.write("Loss_History:\n")
-    f.write(str(loss_history))
+            with torch.no_grad():
+                pred_validation = model(X1_new_valid, X2_new_valid)
+                loss_validation = loss_fn(pred_validation, Y_new_valid)
+                accuracy_validation = accuracy(pred_validation, Y_new_valid)
+                valid_loss.append(loss_validation.item())
+                valid_acc.append(accuracy_validation)
+                print(f"Validation Loss: {loss_validation}, Validation Accuracy: {accuracy_validation}%")
+                if early_stopper.early_stop(loss_validation):
+                    print("Loss converged!")
+                    break
+    
+    with torch.no_grad():
+        pred_test = model(X1_new_test, X2_new_test)
+        loss_test = loss_fn(pred_test, Y_new_test)
+        accuracy_test = accuracy(pred_test, Y_new_test)
+        print(f"Test Loss: {loss_test}, Test Accuracy: {accuracy_test}%")
+
+    f = open(f"Results/{parameters.model} + {parameters.measure}.txt", 'w')
+    f.write("Loss History:\n")
+    f.write(str(train_loss))
+    f.write("\n\n")
+    f.write("Validation Loss History:\n")
+    f.write(str(valid_loss))
+    f.write("\n")
+    f.write("Validation Accuracy History:\n")
+    f.write(str(valid_acc))
+    f.write("\n\n")
+    f.write(f"Test Loss: {loss_test}\n")
+    f.write(f"Test Accuracy: {accuracy_test}\n")
     f.close()
+    torch.save(model.state_dict(), f'Results/{parameters.model} + {parameters.measure}.pt')
 
 
-def train_distance():
-    loss_history = []
-    model = Hybrid_nn.HybridModel_distance().to(device)
+# Train distance models
+def train_hybrid_distance():
+    train_loss, valid_loss = [], []
+    model = Hybrid_nn.get_model().to(device)
     model.train()
 
-    loss_fn = torch.nn.MSELoss()
-    opt = torch.optim.SGD(model.parameters(), lr=0.01)
+    opt = torch.optim.SGD(model.parameters(), lr=0.1)
     for it in range(iterations):
         
-        batch_index = np.random.randint(0, min(len(X1_train), len(X0_train)), (batch_size,))
-        X1_batch, X0_batch = X1_train[batch_index], X0_train[batch_index]
+        batch_index = np.random.randint(0, min(len(X1_train_distance), len(X0_train_distance)), (batch_size,))
+        X1_batch, X0_batch = X1_train_distance[batch_index], X0_train_distance[batch_index]
         X1_batch, X0_batch = torch.tensor(X1_batch).to(device), torch.tensor(X0_batch).to(device)
 
         loss = model(X1_batch, X0_batch)
-        loss_history.append(loss.item())
+        train_loss.append(loss.item())
 
         opt.zero_grad()
         loss.backward()
         opt.step()
 
-        if it % 10 == 0:
+        if it % 20 == 0:
             print(f"Iterations: {it} Loss: {loss.item()}")
+            with torch.no_grad():
+                loss_validation = model(X1_valid_distance, X0_valid_distance)
+                valid_loss.append(loss_validation.item())
+                print(f"Validation Loss: {loss_validation}")
+                if early_stopper.early_stop(loss_validation):
+                    print("Loss converged!")
+                    break
+
+    with torch.no_grad():
+        loss_test = model(X1_test_distance, X0_test_distance)
+        print(f"Test Loss: {loss_test.item()}")
+
+    f = open(f"Results/{parameters.model} + {parameters.measure}.txt", 'w')
+    f.write("Loss History:\n")
+    f.write(str(train_loss))
+    f.write("\n\n")
+    f.write("Validation Loss History:\n")
+    f.write(str(valid_loss))
+    f.write("\n\n")
+    f.write(f"Test Loss: {loss_test.item()}\n")
+    f.close()
+    torch.save(model.state_dict(), f'Results/{parameters.model} + {parameters.measure}.pt')
+
+def train():
+    if parameters.model == 'model1' or parameters.model == 'model2':
+        train_hybrid()
+    else:
+        train_hybrid_distance()
 
 train()
-#train_distance()
